3a8b7642193478201ed04057f41232acd25f666c
JGovardhan@2007 <jyosula.vignan@gmail.com>
2025-03-23 14:52:22 +0000

    Added script.sh for Assignment 4

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export GITHUB_USERNAME="JGovardhan2007"
+export GITHUB_REPOSITORY="cs1102-24f3100193"
+
+echo "GitHub Username: $GITHUB_USERNAME"
+echo "GitHub Repository: $GITHUB_REPOSITORY"
+
